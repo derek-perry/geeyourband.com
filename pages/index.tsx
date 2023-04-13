2a88dfd5a3dd4fc8a83cbf59c1a047da17b8abf1
derek-perry <dp@dp.design>
2023-04-13 13:58:43 -0400

    Increase FB Size
    The FB page embed was increased in size and changed to iframe.

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const homePage: NextPage = () => {
       <h2 id="calendar" className="mt-20 text-4xl font-bold text-rose-400">Stay Up-To-Date</h2>
       <p className="my-4 text-2xl">Find upcoming shows on our Facebook:</p>
       <div className="max-w-[1100px] w-full flex flex-row flex-wrap items-center justify-center text-center">
-        <div id="fb-root"></div><script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0" nonce="AMwHggVR"></script><div className="w-[] fb-page" data-href="https://facebook.com/geeyourbandsmellsterrific" data-tabs="timeline, events" data-width="500" data-height="650" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://facebook.com/geeyourbandsmellsterrific" className="fb-xfbml-parse-ignore"><div className="flex flex-row flex-wrap items-center justify-center text-center"><LinkExternalButton href="https://facebook.com/geeyourbandsmellsterrific" title="GYBST Facebook" size="[200px]">Facebook</LinkExternalButton></div></blockquote></div>
+      <iframe src="https://facebook.com/plugins/page.php?href=https%3A%2F%2facebook.com%2Fgeeyourbandsmellsterrific&tabs=timeline&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" className="h-[700px] w-[700px] border-none overflow-hidden" scrolling="no" frameborder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
       </div>
 
       <h2 id="video" className="mt-20 text-4xl font-bold text-yellow-400">Band Video</h2>
